0db140d7d1c7a7258fe386a7fba0804b9fff3a0a
Ofek Nourian <ooffeekkii@gmail.com>
2025-05-23 09:22:17 +0300

    made network deeper

--- dqn_model.py
+++ dqn_model.py
@@ -9,11 +9,15 @@ class DQN(nn.Module):
     def __init__(self, obs_dim, act_dim):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(obs_dim, 128),
+            nn.Linear(obs_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 128),
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
-            nn.Linear(128, act_dim)
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, act_dim)
         )
 
     def forward(self, x):
